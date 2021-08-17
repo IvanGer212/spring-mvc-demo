ec0e0933adf117ef2e379f11a6b45df2412840c6
IvanGer212 <Comradeivan88@gmail.com>
2021-08-17 11:24:13 +0300

    Add check data from form for add new Product and new Exception and page with exceptions

--- src/main/java/com/geekbrains/ru/springmvcdemo/controller/ProductController.java
+++ src/main/java/com/geekbrains/ru/springmvcdemo/controller/ProductController.java
@@ -2,13 +2,16 @@ package com.geekbrains.ru.springmvcdemo.controller;
 
 
 import com.geekbrains.ru.springmvcdemo.domain.Product;
+import com.geekbrains.ru.springmvcdemo.exception.ErrorResponse;
 import com.geekbrains.ru.springmvcdemo.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/product")
@@ -40,8 +43,28 @@ public class ProductController {
 
     @PostMapping
     public String addProduct(@ModelAttribute("newProduct") Product newProduct, Model model) {
+        Optional<ErrorResponse> validationError = productValidation(newProduct);
+        if(validationError.isPresent()){
+            model.addAttribute("error", validationError.get());
+            return "exception-page";
+        }
             productService.create(newProduct);
         return "redirect:/product";
     }
 
+    private Optional<ErrorResponse> productValidation(Product product) {
+        List<String> details = new ArrayList<>();
+
+        if (product.getTitle().isEmpty()) {
+            details.add("Product name cannot be empty.");
+        }
+        if (product.getCost() <= 0) {
+            details.add("Price cannot be less then 0.");
+        }
+        if(!details.isEmpty()){
+            return Optional.of(new ErrorResponse("Validation failed!", details));
+        }
+
+        return Optional.empty();
+    }
 }
